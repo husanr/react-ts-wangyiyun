55ef08e30a225f55cb9d5aeebf20598e51209403
husanr <1090037611@qq.com>
2023-11-28 15:30:00 +0800

    fix: 将craco.config.ts改为js

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,13 @@
 const path = require('path')
 const resolve = (dir) => path.resolve(__dirname, dir)
+const cracoLessPlugin = require('craco-less')
 
 module.exports = {
+  plugins: [
+    {
+      plugin: cracoLessPlugin
+    }
+  ],
   webpack: {
     alias: {
       '@': resolve('src')
