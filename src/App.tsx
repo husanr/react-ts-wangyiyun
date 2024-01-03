79536a21524b27477aee7f38a42d929cbe01a3c6
husanr <1090037611@qq.com>
2024-01-03 16:29:32 +0800

    feat: 新增header和footer组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React, { Suspense } from 'react'
 import { useRoutes, Link } from 'react-router-dom'
 import routes from './router'
-import Demo1 from './views/demo/demo1'
+import AppHeader from './components/app-header'
+import AppFooter from './components/app-footer'
+// import Demo1 from './views/demo/demo1'
 
 function App() {
   return (
     <Suspense fallback="">
-      <Demo1 name="Lily" age={10} />
-      <nav>
-        <Link to="/discover">发现音乐</Link>
-        <Link to="/mine">我的音乐</Link>
-        <Link to="/focus">关注</Link>
-        <Link to="/download">下载客户端</Link>
-      </nav>
+      {/* <Demo1 name="Lily" age={10} /> */}
+      <AppHeader />
       <div>{useRoutes(routes)}</div>
+      <AppFooter />
     </Suspense>
   )
 }
---
+++ src/components/app-footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import type { FC, ReactNode } from 'react'
+
+interface IProps {
+  children?: ReactNode
+}
+
+const AppFooter: FC<IProps> = () => {
+  return <div>AppFooter</div>
+}
+
+export default AppFooter
