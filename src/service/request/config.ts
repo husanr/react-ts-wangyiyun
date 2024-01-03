86a58a75c445d489043a1e52698b03b7fd98a275
husanr <1090037611@qq.com>
2024-01-03 10:48:30 +0800

    chore: 配置环境变量

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://codercba.com:9002'
+// const BASE_URL = 'http://codercba.com:9002'
+const BASE_URL = process.env.REACT_APP_BASE_URL
 const TIME_OUT = 10000
 
 export { BASE_URL, TIME_OUT }
