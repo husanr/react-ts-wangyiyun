04f2411d4ebcdbb0b5d52cd57b14fbaa4883d6c1
husanr <1090037611@qq.com>
2024-01-08 10:03:23 +0800

    fix: 引入banner组件，banner组件中获取store数据并展示

--- src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import type { FC, ReactNode } from 'react'
 import { BannerWrapper } from './style'
+import { useAppSelector } from '@/store'
+import { shallowEqual } from 'react-redux'
 
 interface IProps {
   children?: ReactNode
 }
 
 const TopBanner: FC<IProps> = () => {
-  return <BannerWrapper>TopBanner</BannerWrapper>
+  const { banners } = useAppSelector(
+    (state) => ({
+      banners: state.recommend.banners
+    }),
+    shallowEqual
+  )
+  return (
+    <BannerWrapper>
+      {banners.map((item) => {
+        return (
+          <div key={item.encodeId}>
+            <img src={item.imageUrl} alt="" />
+          </div>
+        )
+      })}
+    </BannerWrapper>
+  )
 }
 
 export default TopBanner
--- src/views/discover/c-views/recommend/index.tsx
+++ src/views/discover/c-views/recommend/index.tsx
@@ -2,6 +2,7 @@ import { useAppDispatch } from '@/store'
 import React, { useEffect } from 'react'
 import type { FC, ReactNode } from 'react'
 import { fetchBannerThunk } from './store'
+import TopBanner from './c-cpns/top-banner'
 
 interface IProps {
   children?: ReactNode
@@ -12,7 +13,11 @@ const Recommend: FC<IProps> = () => {
   useEffect(() => {
     dispatch(fetchBannerThunk())
   }, [])
-  return <div>Recommend</div>
+  return (
+    <div>
+      <TopBanner />
+    </div>
+  )
 }
 
 export default Recommend
