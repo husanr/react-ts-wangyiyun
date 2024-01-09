ebeb935a9c5dd12ea4efd9c392cdcb76c0ffb294
husanr <2581147750@qq.com>
2024-01-09 21:47:53 +0800

    fix: rankingitem完善

--- src/views/discover/c-views/recommend/c-cpns/top-ranking-item/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/top-ranking-item/index.tsx
@@ -1,14 +1,53 @@
 import React from 'react'
 import type { FC, ReactNode } from 'react'
 import { TopRankItemWrapper } from './style'
+import { getImgSize } from '@/utils/format'
 
 interface IProps {
   children?: ReactNode
   itemData: any
 }
 
-const TopRankingItem: FC<IProps> = () => {
-  return <TopRankItemWrapper>TopRankingItem</TopRankItemWrapper>
+const TopRankingItem: FC<IProps> = (props) => {
+  const { itemData } = props
+  const { tracks = [] } = itemData
+  return (
+    <TopRankItemWrapper>
+      <div className="header">
+        <div className="image">
+          <img src={getImgSize(itemData.coverImgUrl, 100)} alt="" />
+          <a href="" className="sprite_cover header_cover"></a>
+        </div>
+        <div className="info">
+          <div className="name">{itemData.name}</div>
+          <div>
+            <button className="sprite_02 btn play"></button>
+            <button className="sprite_02 btn favor"></button>
+          </div>
+        </div>
+      </div>
+      <div className="list">
+        {tracks.slice(0, 10).map((item: any, index: number) => {
+          return (
+            <div className="item" key={item.id}>
+              <div className="index">{index + 1}</div>
+              <div className="title">
+                <div className="song_name">{item.name}</div>
+                <div className="operator">
+                  <button className="btn sprite_02 play"></button>
+                  <button className="btn sprite_icon2 add"></button>
+                  <button className="btn sprite_02 favor"></button>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      <div className="footer">
+        <a href="#/discover/ranking">查看全部&gt;</a>
+      </div>
+    </TopRankItemWrapper>
+  )
 }
 
 export default TopRankingItem
--- src/views/discover/c-views/recommend/c-cpns/top-ranking-item/style.ts
+++ src/views/discover/c-views/recommend/c-cpns/top-ranking-item/style.ts
@@ -5,4 +5,166 @@ export const TopRankItemWrapper = styled.div`
   &:last-child {
     width: 228px;
   }
+
+  .header {
+    height: 100px;
+    display: flex;
+
+    margin: 20px 0 0 20px;
+
+    .image {
+      width: 80px;
+      height: 80px;
+      position: relative;
+
+      img {
+        width: 80px;
+        height: 80px;
+      }
+      .header_cover {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-position: -145px -57px;
+      }
+    }
+
+    .info {
+      margin: 5px 0 0 10px;
+
+      .name {
+        font-size: 14px;
+        font-weight: 700;
+        color: #333;
+        &:hover {
+          cursor: pointer;
+          text-decoration: underline;
+        }
+      }
+
+      a {
+        font-size: 14px;
+        color: #333;
+        font-weight: 700;
+      }
+
+      .btn {
+        display: inline-block;
+        text-indent: -9999px;
+        width: 22px;
+        height: 22px;
+        margin: 8px 10px 0 0;
+        cursor: pointer;
+      }
+
+      .play {
+        background-position: -267px -205px;
+
+        &:hover {
+          background-position: -267px -235px;
+        }
+      }
+
+      .favor {
+        background-position: -300px -205px;
+
+        &:hover {
+          background-position: -300px -235px;
+        }
+      }
+    }
+  }
+
+  .list {
+    .item {
+      position: relative;
+      display: flex;
+      align-items: center;
+      height: 32px;
+
+      &:nth-child(-n + 3) .index {
+        color: #c10d0c;
+      }
+
+      .index {
+        width: 35px;
+        text-align: center;
+        margin-left: 10px;
+        font-size: 16px;
+      }
+
+      .title {
+        color: #000;
+        width: 170px;
+        height: 17px;
+        line-height: 17px;
+        display: flex;
+        justify-content: space-between;
+
+        .song_name {
+          flex: 1;
+
+          ${(props) => props.theme.mixin.textNowrap}
+          cursor: pointer;
+
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+
+        .operator {
+          display: flex;
+          align-items: center;
+          display: none;
+          width: 82px;
+
+          .btn {
+            width: 17px;
+            height: 17px;
+            margin-left: 8px;
+            cursor: pointer;
+          }
+
+          .play {
+            background-position: -267px -268px;
+            &:hover {
+              background-position: -267px -288px;
+            }
+          }
+
+          .add {
+            position: relative;
+            top: 2px;
+            background-position: 0 -700px;
+            &:hover {
+              background-position: -22px -700px;
+            }
+          }
+
+          .favor {
+            background-position: -297px -268px;
+            &:hover {
+              background-position: -297px -288px;
+            }
+          }
+        }
+      }
+
+      &:hover {
+        .operator {
+          display: block;
+        }
+      }
+    }
+  }
+
+  .footer {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    height: 32px;
+    padding-right: 20px;
+  }
 `
