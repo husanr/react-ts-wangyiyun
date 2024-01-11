4d3c667572fc462d1f28e4e3661568bbf01211da
husanr <1090037611@qq.com>
2024-01-11 16:45:47 +0800

    chore: 实现播放效果

--- src/views/player/app-player-bar/index.tsx
+++ src/views/player/app-player-bar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import type { FC, ReactNode } from 'react'
 import { BarControl, BarFavor, BarInfo, PlayerBarWrapper } from './style'
 import { Link } from 'react-router-dom'
@@ -6,25 +6,73 @@ import { Slider } from 'antd'
 import { useAppSelector } from '@/store'
 import { shallowEqual } from 'react-redux'
 import { getImgSize } from '@/utils/format'
+import { getPlayUrl } from '@/utils/player'
 
 interface IProps {
   children?: ReactNode
 }
 
 const AppPlayerBar: FC<IProps> = () => {
+  const audioRef = useRef<HTMLAudioElement>(null)
   const [isPlaying, setIsPlaying] = useState(false)
+  const [progress, setProgress] = useState(0)
+  const [duration, setDuration] = useState(0)
+
   const { currentSong } = useAppSelector(
     (state) => ({
       currentSong: state.player.currentSong
     }),
     shallowEqual
   )
+
+  // 副作用
+  useEffect(() => {
+    // 播放音乐
+    audioRef.current!.src = getPlayUrl(2101373186)
+    // audioRef.current
+    //   ?.play()
+    //   .then(() => {
+    //     console.log('播放成功')
+    //     setIsPlaying(true)
+    //   })
+    //   .catch((error) => {
+    //     setIsPlaying(false)
+    //     console.log('播放失败', error)
+    //   })
+
+    // 设置总时长
+    setDuration(currentSong.dt)
+  }, [currentSong])
+
+  const handlePlayBtnClick = () => {
+    console.log(isPlaying ? '播放' : '暂停')
+    // 播放和暂停
+    isPlaying
+      ? audioRef.current?.pause()
+      : audioRef.current?.play().catch(() => setIsPlaying(false))
+
+    setIsPlaying(!isPlaying)
+  }
+
+  const handleTimeUpdate = () => {
+    console.log('update')
+    const currentTime = audioRef.current!.currentTime * 1000
+    const progress = (currentTime / duration) * 100
+    setProgress(progress)
+  }
+
+  const handleTimeEnded = () => {
+    console.log('ended')
+  }
   return (
     <PlayerBarWrapper className="sprite_playbar">
       <div className="content wrap-v2">
         <BarControl isPlaying={isPlaying}>
           <button className="btn sprite_playbar prev"></button>
-          <button className="btn sprite_playbar play"></button>
+          <button
+            className="btn sprite_playbar play"
+            onClick={handlePlayBtnClick}
+          ></button>
           <button className="btn sprite_playbar next"></button>
         </BarControl>
         <BarInfo>
@@ -41,7 +89,11 @@ const AppPlayerBar: FC<IProps> = () => {
               <span className="singer_name">{currentSong.ar[0].name}</span>
             </div>
             <div className="progress">
-              <Slider />
+              <Slider
+                value={progress}
+                step={0.5}
+                tooltip={{ formatter: null }}
+              />
               <div className="time">
                 <span className="current">00:11</span>
                 <span className="divider">/</span>
@@ -63,6 +115,11 @@ const AppPlayerBar: FC<IProps> = () => {
           </div>
         </BarFavor>
       </div>
+      <audio
+        ref={audioRef}
+        onTimeUpdate={handleTimeUpdate}
+        onEnded={handleTimeEnded}
+      />
     </PlayerBarWrapper>
   )
 }
